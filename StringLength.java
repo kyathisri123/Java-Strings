490ba908bf21c84c1ae4af8c2ceda271a294ad36
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:40:30 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Length of String: " + str.length());
+    }
+}
+
