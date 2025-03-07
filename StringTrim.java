6dcc75423be8c3929688308c0bf123421a9d5158
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:42:28 +0530

    Add files via upload

---
+++ StringTrim.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class StringTrim {
+    public static void main(String[] args) {
+        String str = "   Hello World   ";
+        System.out.println("Trimmed String: '" + str.trim() + "'");
+    }
+}
