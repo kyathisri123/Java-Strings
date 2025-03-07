5703f6d6316a687acccef963170276cc1635bff4
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:41:30 +0530

    Add files via upload

---
+++ StringSearch.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringSearch {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int index = str.indexOf("World");
+        System.out.println("Index of 'World': " + index);
+    }
+}
+
