f34db978cc6ad612ef7737ad852a98378d4e9344
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:42:46 +0530

    Add files via upload

---
+++ StringValueOf.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringValueOf {
+    public static void main(String[] args) {
+        int number = 123;
+        String str = String.valueOf(number);
+        System.out.println("Converted String: " + str);
+    }
+}
+
