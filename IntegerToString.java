024b76afd4d8728339a68e3b44cde67fc792d863
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:38:49 +0530

    Add files via upload

---
+++ IntegerToString.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class IntegerToString {
+    public static void main(String[] args) {
+        Integer num = 456;
+        String str = num.toString();
+        System.out.println("Converted String: " + str);
+    }
+}
+
