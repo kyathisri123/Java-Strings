bf01ae76bec5cba42ee4724821921abee704d9ec
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:42:12 +0530

    Add files via upload

---
+++ StringSubstring.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringSubstring {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Substring (0-5): " + str.substring(0, 5));
+    }
+}
+
