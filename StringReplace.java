71748d62dd19c6946457dd1aa6f38a02584ca404
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:41:11 +0530

    Add files via upload

---
+++ StringReplace.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringReplace {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Replaced String: " + str.replace("World", "Java"));
+    }
+}
+
